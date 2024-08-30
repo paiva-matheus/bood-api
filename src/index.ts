32a1dfbb80d34b8aa48bd4799abf2a9735d7d0ea
Matheus Paiva <matheuspaivah2@gmail.com>
2024-08-30 15:36:38 -0300

    update config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import app from '@/lib/createServer';
 
-app.listen(8000, () => console.log(`Server ready at: http://localhost:8000`));
+app.listen(
+  { host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 8000 },
+  () => console.log(`Server ready at: http://localhost:8000`)
+);
